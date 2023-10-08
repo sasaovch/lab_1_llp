e7101afbe6512ff40c341f4a38b33ba1cd952c2c
sasaovch <sasaovch@gmail.com>
2023-10-08 13:38:38 +0300

    Worked all types

--- main.c
+++ main.c
@@ -7,7 +7,9 @@
 #include <string.h>
 #include <stdarg.h>
 
-const uint32_t PAGE_SIZE = 32;
+//FIXME: подумать о слишком больших данных
+
+const uint32_t PAGE_SIZE = 48;
 const uint32_t PAGE_HEADER_SIZE = 12;
 const uint32_t PAGE_BODY_SIZE = PAGE_SIZE - PAGE_HEADER_SIZE;
 const uint32_t NAME_TYPE_SIZE = 6;
@@ -190,7 +192,6 @@ void save_meta_page_and_create_new(Cursor* cursor) {
     page->page_header = page_header;
     page->body = malloc(PAGE_BODY_SIZE);;
     cursor->page = page;
-    printf("Save and create new page\n");
 }
 
 void create_page_for_type(Cursor* cursor) {
@@ -207,14 +208,11 @@ void create_page_for_type(Cursor* cursor) {
 
     free(pageHeader);
     free(page_for_type);
-    
-    printf("Created type page\n");
 }
 
 void save_type(Cursor* cursor, MetaTable* meta_page) {
     // Если в текущем блоке не хватает места, то нужно сохранить блок в память и создать новый
     if (cursor->page->page_header->offset + META_TABLE_SIZE > PAGE_SIZE) {
-        printf("Need to create new page\n");
         save_meta_page_and_create_new(cursor);
     }
 
@@ -232,17 +230,37 @@ void save_type(Cursor* cursor, MetaTable* meta_page) {
 
 void print_type(MetaTable* meta_page) {
     printf("Print\n");
-    printf("%i\n", meta_page->type);
-    printf("%i\n", meta_page->first_block);
-    printf("%i\n", meta_page->last_block);
-    printf("%s\n", meta_page->name_type);
+    printf("Type - %i\n", meta_page->type);
+    printf("First - %i\n", meta_page->first_block);
+    printf("Last - %i\n", meta_page->last_block);
+    printf("Name - %s\n", meta_page->name_type);
 }
 
 void print_node(Node* node) {
     printf("Print node\n");
-    printf("%s\n", node->name_type);
-    printf("%i\n", node->name_length);
-    printf("%s\n", node->name);
+    printf("Type - %s\n", node->name_type);
+    printf("Length - %i\n", node->name_length);
+    printf("Name - %s\n", node->name);
+}
+
+void print_relationship(Relationship* node) {
+    printf("Print relationship\n");
+    printf("Type - %s\n", node->name_type);
+    printf("Parent length - %i\n", node->parent_length);
+    printf("Parent - %s\n", node->parent);
+    printf("Child length - %i\n", node->child_length);
+    printf("Child - %s\n", node->child);
+}
+
+void print_property(Property* node) {
+    printf("Print property\n");
+    printf("Subject length - %i\n", node->subject_length);
+    printf("Subject - %s\n", node->subject);
+    printf("Key length - %i\n", node->key_length);
+    printf("Key - %s\n", node->key);
+    printf("Value length - %i\n", node->value_length);
+    printf("Value - %s\n", node->value);
+    
 }
 
 MetaTable* get_meta_table(Cursor* cursor, TypeOfElement type, const char* name, uint64_t* pointer) {
@@ -298,7 +316,6 @@ void create_node(Cursor* cursor, Node* node) {
     uint64_t pointer = 0;
 
     MetaTable* table = get_meta_table(cursor, NODE, node->name_type, &(pointer));
-    print_type(table);
     if (table == NULL) {
         printf("Error to find Node with name %s", node->name_type);
         return;
@@ -381,8 +398,7 @@ void read_all_nodes(Cursor* cursor) {
         
         for (int i = 0; i < (pageHeader->offset / META_TABLE_SIZE); i++) {
             memcpy(metaPage, page_body + i * META_TABLE_SIZE, META_TABLE_SIZE);
-            print_type(metaPage);
-            read_nodes_by_type(cursor, metaPage);
+            if (metaPage->type == NODE) read_nodes_by_type(cursor, metaPage);
         }
         page_num++;
     } while (page_num * PAGE_SIZE < file_length);
@@ -413,20 +429,271 @@ void read_all_types(Cursor* cursor) {
     } while (page_num * PAGE_SIZE < file_length);
 }
 
+void create_relationship_page(Cursor* cursor, PageHeader* pageHeader, uint64_t curr_table_page_offset) {
+    // Записать в текущий блок номер следующего блока
+    cursor->number_of_pages++;
+
+    set_pointer_offset_file(cursor->file, curr_table_page_offset + sizeof(uint32_t));
+    write_to_file(cursor->file, &(cursor->number_of_pages), sizeof(uint32_t));
+
+    void* new_page = malloc(PAGE_SIZE);
+
+    pageHeader->block_number = cursor->number_of_pages;
+    pageHeader->offset = 0;
+    pageHeader->next_block = 0;
+
+    memcpy(new_page, pageHeader, PAGE_HEADER_SIZE);
+    
+    set_pointer_offset_file(cursor->file, cursor->number_of_pages * PAGE_SIZE);
+    write_to_file(cursor->file, new_page, PAGE_SIZE);
+}
+
+void create_relationship(Cursor* cursor, Relationship* relationship) {
+    uint64_t pointer = 0;
+
+    MetaTable* table = get_meta_table(cursor, RELATIONSHIP, relationship->name_type, &(pointer));
+    if (table == NULL) {
+        printf("Error to find Node with name %s", relationship->name_type);
+        return;
+    }
+
+    uint32_t page_num = table->last_block;
+    uint32_t page_offset = page_num * PAGE_SIZE;
+
+    PageHeader* pageHeader = (PageHeader*) malloc(PAGE_HEADER_SIZE);
+
+    set_pointer_offset_file(cursor->file, page_offset);
+    read_from_file(cursor->file, pageHeader, PAGE_HEADER_SIZE);
+
+    uint32_t global_offset = page_offset + pageHeader->offset + PAGE_HEADER_SIZE;
+
+    if (pageHeader->offset + sizeof(uint32_t) * 2 + sizeof(char) * (relationship->child_length + relationship->parent_length) > PAGE_BODY_SIZE) {
+        create_relationship_page(cursor, pageHeader, page_offset);
+        
+        table->last_block = cursor->number_of_pages;
+        global_offset = cursor->number_of_pages * PAGE_SIZE + PAGE_HEADER_SIZE;
+        
+        // Записать номер последнего блока для мета таблицы
+        set_pointer_offset_file(cursor->file, pointer + sizeof(TypeOfElement) + sizeof(uint32_t));
+        write_to_file(cursor->file, &(table->last_block), sizeof(uint32_t));
+    }
+
+    set_pointer_offset_file(cursor->file, global_offset);
+    write_to_file(cursor->file, &(relationship->parent_length), sizeof(uint32_t));
+    write_to_file(cursor->file, relationship->parent, sizeof(char) * relationship->parent_length);
+    write_to_file(cursor->file, &(relationship->child_length), sizeof(uint32_t));
+    write_to_file(cursor->file, relationship->child, sizeof(char) * relationship->child_length);
+
+    uint32_t header_offset = global_offset - pageHeader->offset - PAGE_HEADER_SIZE + sizeof(uint32_t) * 2;
+
+    pageHeader->offset += sizeof(uint32_t) * 2 + sizeof(char) * (relationship->child_length + relationship->parent_length);
+
+    set_pointer_offset_file(cursor->file, header_offset);
+    write_to_file(cursor->file, &(pageHeader->offset), sizeof(uint32_t));
+}
+
+void read_relationship_by_type(Cursor* cursor, MetaTable* metaPage) {
+    uint32_t read_block = metaPage->first_block;
+    PageHeader* pageHeader = (PageHeader*) malloc(PAGE_HEADER_SIZE);
+    void* body = malloc(PAGE_BODY_SIZE);
+    while (read_block != 0) {
+        uint32_t page_offset = read_block * PAGE_SIZE;
+        set_pointer_offset_file(cursor->file, page_offset);
+        read_from_file(cursor->file, pageHeader, PAGE_HEADER_SIZE);  
+        read_from_file(cursor->file, body, PAGE_BODY_SIZE);
+        uint32_t offset = 0;
+        Relationship* relationship = (Relationship*) malloc(sizeof(Relationship));
+        strcpy(relationship->name_type, metaPage->name_type);
+        while (offset != pageHeader->offset) {
+            uint32_t parent_length = 0;
+            uint32_t child_length = 0;
+            memcpy(&(parent_length), body + offset, sizeof(uint32_t));
+            char* parent = malloc(sizeof(char) * parent_length);
+            memcpy(parent, body + sizeof(uint32_t) + offset, sizeof(char) * parent_length);
+            
+            memcpy(&(child_length), body + offset + sizeof(uint32_t) + sizeof(char) * parent_length, sizeof(uint32_t));
+            char* child = malloc(sizeof(char) * child_length);
+            memcpy(child, body + sizeof(uint32_t) + offset + sizeof(uint32_t) + sizeof(char) * parent_length, sizeof(char) * child_length);
+            
+            relationship->parent_length = parent_length;
+            relationship->parent = parent;
+            relationship->child_length = child_length;
+            relationship->child = child;
+            offset += sizeof(char) * (parent_length + child_length) + 2 * sizeof(uint32_t);
+            print_relationship(relationship);
+        }
+        read_block = pageHeader->next_block;
+    }
+}
+
+void read_all_relationship(Cursor* cursor) {
+    MetaTable* metaPage = (MetaTable*) malloc(META_TABLE_SIZE);
+    PageHeader* pageHeader = (PageHeader*) malloc(PAGE_HEADER_SIZE);
+    
+    void* page_body = malloc(PAGE_BODY_SIZE);
+    int page_num = 0;
+
+    off_t file_length = lseek(cursor->file->file_descriptor, 0, SEEK_END); // find end of file
+    error_exit(file_length, "Failed to find end of file");
+    
+    do {
+        set_pointer_offset_file(cursor->file, page_num * PAGE_SIZE);
+
+        read_from_file(cursor->file, pageHeader, PAGE_HEADER_SIZE);        
+        read_from_file(cursor->file, page_body, PAGE_BODY_SIZE);
+        
+        for (int i = 0; i < (pageHeader->offset / META_TABLE_SIZE); i++) {
+            memcpy(metaPage, page_body + i * META_TABLE_SIZE, META_TABLE_SIZE);
+            if (metaPage->type == RELATIONSHIP) read_relationship_by_type(cursor, metaPage);
+        }
+        page_num++;
+    } while (page_num * PAGE_SIZE < file_length);
+}
+
+void create_property_page(Cursor* cursor, PageHeader* pageHeader, uint64_t curr_table_page_offset) {
+    // Записать в текущий блок номер следующего блока
+    cursor->number_of_pages++;
+
+    set_pointer_offset_file(cursor->file, curr_table_page_offset + sizeof(uint32_t));
+    write_to_file(cursor->file, &(cursor->number_of_pages), sizeof(uint32_t));
+
+    void* new_page = malloc(PAGE_SIZE);
+
+    pageHeader->block_number = cursor->number_of_pages;
+    pageHeader->offset = 0;
+    pageHeader->next_block = 0;
+
+    memcpy(new_page, pageHeader, PAGE_HEADER_SIZE);
+    
+    set_pointer_offset_file(cursor->file, cursor->number_of_pages * PAGE_SIZE);
+    write_to_file(cursor->file, new_page, PAGE_SIZE);
+}
+
+void create_property(Cursor* cursor, Property* property) {
+    uint64_t pointer = 0;
+
+    MetaTable* table = get_meta_table(cursor, PROPERTY, "proper", &(pointer));
+    if (table == NULL) {
+        printf("Error to find Node with name %s", "proper");
+        return;
+    }
+
+    uint32_t page_num = table->last_block;
+    uint32_t page_offset = page_num * PAGE_SIZE;
+
+    PageHeader* pageHeader = (PageHeader*) malloc(PAGE_HEADER_SIZE);
+
+    set_pointer_offset_file(cursor->file, page_offset);
+    read_from_file(cursor->file, pageHeader, PAGE_HEADER_SIZE);
+
+    uint32_t global_offset = page_offset + pageHeader->offset + PAGE_HEADER_SIZE;
+
+    if (pageHeader->offset + sizeof(uint32_t) * 3 + sizeof(char) * (property->subject_length + property->key_length + property->value_length) > PAGE_BODY_SIZE) {
+        create_property_page(cursor, pageHeader, page_offset);
+        
+        table->last_block = cursor->number_of_pages;
+        global_offset = cursor->number_of_pages * PAGE_SIZE + PAGE_HEADER_SIZE;
+        
+        // Записать номер последнего блока для мета таблицы
+        set_pointer_offset_file(cursor->file, pointer + sizeof(TypeOfElement) + sizeof(uint32_t));
+        write_to_file(cursor->file, &(table->last_block), sizeof(uint32_t));
+    }
+
+    set_pointer_offset_file(cursor->file, global_offset);
+    write_to_file(cursor->file, &(property->subject_length), sizeof(uint32_t));
+    write_to_file(cursor->file, property->subject, sizeof(char) * property->subject_length);
+    write_to_file(cursor->file, &(property->key_length), sizeof(uint32_t));
+    write_to_file(cursor->file, property->key, sizeof(char) * property->key_length);
+    write_to_file(cursor->file, &(property->value_length), sizeof(uint32_t));
+    write_to_file(cursor->file, property->value, sizeof(char) * property->value_length);
+
+    uint32_t header_offset = global_offset - pageHeader->offset - PAGE_HEADER_SIZE + sizeof(uint32_t) * 2;
+
+    pageHeader->offset += sizeof(uint32_t) * 3 + sizeof(char) * (property->subject_length + property->key_length + property->value_length);
+
+    set_pointer_offset_file(cursor->file, header_offset);
+    write_to_file(cursor->file, &(pageHeader->offset), sizeof(uint32_t));
+}
+
+void read_property_by_type(Cursor* cursor, MetaTable* metaPage) {
+    uint32_t read_block = metaPage->first_block;
+    PageHeader* pageHeader = (PageHeader*) malloc(PAGE_HEADER_SIZE);
+    void* body = malloc(PAGE_BODY_SIZE);
+    while (read_block != 0) {
+        uint32_t page_offset = read_block * PAGE_SIZE;
+        set_pointer_offset_file(cursor->file, page_offset);
+        read_from_file(cursor->file, pageHeader, PAGE_HEADER_SIZE);  
+        read_from_file(cursor->file, body, PAGE_BODY_SIZE);
+        uint32_t offset = 0;
+        Property* property = (Property*) malloc(sizeof(Property));
+        // strcpy(property->name_type, metaPage->name_type);
+        while (offset != pageHeader->offset) {
+            uint32_t subject_length = 0;
+            uint32_t key_length = 0;
+            uint32_t value_length = 0;
+
+            memcpy(&(subject_length), body + offset, sizeof(uint32_t));
+            char* subject = malloc(sizeof(char) * subject_length);
+            memcpy(subject, body + offset + sizeof(uint32_t) , sizeof(char) * subject_length);
+            
+            memcpy(&(key_length), body + offset + sizeof(uint32_t) + sizeof(char) * subject_length, sizeof(uint32_t));
+            char* key = malloc(sizeof(char) * key_length);
+            memcpy(key, body + offset + sizeof(uint32_t) + sizeof(uint32_t) + sizeof(char) * subject_length, sizeof(char) * key_length);
+            
+            memcpy(&(value_length), body + offset + sizeof(uint32_t) + sizeof(uint32_t) + sizeof(char) * (subject_length + key_length), sizeof(uint32_t));
+            char* value = malloc(sizeof(char) * value_length);
+            memcpy(value, body + offset + sizeof(uint32_t) + sizeof(uint32_t) + sizeof(uint32_t) + sizeof(char) * (subject_length + key_length), sizeof(char) * value_length);
+            
+            property->subject_length = subject_length;
+            property->subject = subject;
+            property->key_length = key_length;
+            property->key = key;
+            property->value_length = value_length;
+            property->value = value;
+            offset += sizeof(char) * (subject_length + key_length + value_length) + 3 * sizeof(uint32_t);
+            print_property(property);
+        }
+        read_block = pageHeader->next_block;
+    }
+}
+
+void read_all_property(Cursor* cursor) {
+    MetaTable* metaPage = (MetaTable*) malloc(META_TABLE_SIZE);
+    PageHeader* pageHeader = (PageHeader*) malloc(PAGE_HEADER_SIZE);
+    
+    void* page_body = malloc(PAGE_BODY_SIZE);
+    int page_num = 0;
+
+    off_t file_length = lseek(cursor->file->file_descriptor, 0, SEEK_END); // find end of file
+    error_exit(file_length, "Failed to find end of file");
+    
+    do {
+        set_pointer_offset_file(cursor->file, page_num * PAGE_SIZE);
+
+        read_from_file(cursor->file, pageHeader, PAGE_HEADER_SIZE);        
+        read_from_file(cursor->file, page_body, PAGE_BODY_SIZE);
+        
+        for (int i = 0; i < (pageHeader->offset / META_TABLE_SIZE); i++) {
+            memcpy(metaPage, page_body + i * META_TABLE_SIZE, META_TABLE_SIZE);
+            if (metaPage->type == PROPERTY) read_property_by_type(cursor, metaPage);
+        }
+        page_num++;
+    } while (page_num * PAGE_SIZE < file_length);
+}
+
 void insert_meta_table(Cursor* cursor) {
     MetaTable data[] = {
         {NODE, 0, 0, "person"},
         {NODE, 0, 0, "pc"},
         { RELATIONSHIP, 0, 0, "play"},
-        { RELATIONSHIP, 0, 0, "brother"},
+        { RELATIONSHIP, 0, 0, "brothe"},
         {PROPERTY, 0, 0, "proper"},
     };
 
     int count_to_write = sizeof(data) / META_TABLE_SIZE;
-    printf("Start saving\n");
     for (int i = 0; i < count_to_write; i++) {
         save_type(cursor, &(data[i]));
-        printf("Saved %i\n", i);
+        printf("Saved table - %i\n", i);
     }
     flush_page(cursor);
 }
@@ -440,10 +707,9 @@ void insert_node(Cursor* cursor) {
     };
 
     int count_to_write = sizeof(nodes) / sizeof(Node);
-    printf("Start saving node\n");
     for (int i = 0; i < count_to_write; i++) {
         create_node(cursor, &(nodes[i]));
-        printf("Saved node %i\n", i);
+        printf("Saved node - %i\n", i);
     }
     // flush_page(cursor);
 }
@@ -452,9 +718,15 @@ void insert_relationship(Cursor* cursor) {
     Relationship relationships[] = {
         {"play", 8, "sasaovch", 3, "mac"},
         {"play", 6, "andrey", 4, "asus"},
-        {"brother", 8, "sasaovch", 6, "andrey"},
-        {"brother", 6, "andrey", 8, "sasaovch"},
+        {"brothe",8, "sasaovch", 3, "and"},
+        {"brothe", 6, "andrey", 4, "sasa"},
     };
+    
+    int count_to_write = sizeof(relationships) / sizeof(Relationship);
+    for (int i = 0; i < count_to_write; i++) {
+        create_relationship(cursor, &(relationships[i]));
+        printf("Saved relationship - %i\n", i);
+    }
 }
 
 void insert_property(Cursor* cursor) {
@@ -464,25 +736,14 @@ void insert_property(Cursor* cursor) {
         {3, "mac", 5, "color", 6, "silver"},
         {4, "asus", 5, "color", 5, "black"}
     };
+
+        
+    int count_to_write = sizeof(property) / sizeof(Property);
+    for (int i = 0; i < count_to_write; i++) {
+        create_property(cursor, &(property[i]));
+        printf("Saved property - %i\n", i);
+    }
 }
-
-// void select_types(Cursor* cursor) {
-//     cursor->offset = 0;
-//     MetaTable* meta_page = malloc(META_TABLE_SIZE);
-//     read_all_types(cursor, meta_page, NODE);
-//     print_type(meta_page);
-//     free(meta_page);
-
-//     meta_page = malloc(META_TABLE_SIZE);
-//     read_types(cursor, meta_page, RELATIONSHIP);
-//     print_type(meta_page);
-//     free(meta_page);
-
-//     meta_page = malloc(META_TABLE_SIZE);
-//     read_types(cursor, meta_page, PROPERTY);
-//     print_type(meta_page);
-//     free(meta_page);
-// }
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -501,6 +762,14 @@ int main(int argc, char* argv[]) {
     insert_node(cursor);
     printf("Inserted nodes\n");
     read_all_nodes(cursor);
+    
+    insert_relationship(cursor);
+    printf("Inserted relationship\n");
+    read_all_relationship(cursor);
+    
+    insert_property(cursor);
+    printf("Inserted property\n");
+    read_all_property(cursor);
 
 
     // int fd = open("test",
